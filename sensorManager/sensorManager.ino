c870c54f094f9cfd0f1d8371941f6695722434b3
Isaac de Cuba <isaacjdecuba@gmail.com>
2015-11-10 12:03:49 +0100

    WIP - fail to convert sensor float to string for json

--- sensorManager/sensorManager.ino
+++ sensorManager/sensorManager.ino
@@ -10,8 +10,9 @@ int defaultDelay = 2000;
 int id = 1; // id of the data that read by sensors
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-char serverName[] = "45.55.40.47/planten"; // server domain
-String ipServer = "45.55.40.47/planten";
+//char serverName[] = "45.55.40.47/planten"; // server domain
+IPAddress server(45,55,40,47);
+//String ipServer = "45.55.40.47/planten";
 int serverPort = 80; // server\"s port
 char pageName[] = "/create"; // page on the server
 IPAddress ip(192, 168, 0, 177); // Set the static IP address to use if the DHCP fails to assign
@@ -53,17 +54,7 @@ void setup()
   }
 
   delay(defaultDelay);
-  Serial.println(F("connecting..."));
-  
-  // if you get a connection, report back via serial:
-//  if (client.connect("45.55.40.47/planten/create", serverPort)) {
-//    Serial.println("connected");
-//  } else {
-//    Serial.println("connection failed");    // if you didn\"t get a connection to the server:
-//    delay(defaultDelay);
-//    exit(0); // Exit the program
-//  }
-  
+  Serial.println(F("connecting..."));  
   dht.begin();
 }
 
@@ -85,6 +76,7 @@ void loop()
   jsonRequest = "";
 
   id = id + 1;
+  while(true);
 }
 
 void grondMeting() {
@@ -96,9 +88,7 @@ void grondMeting() {
   int sensorInt = (int) sensor0P;
   Serial.print("Ground Humidity Procent: "); //vochtigheid van water is max 80 procent
   Serial.println("");
-  Serial.println("DEBUGGING....." + jsonRequest);
-  jsonRequest = jsonRequest + "\"ground_humidity\": [{ \"value\": 80, \"timestamp\": 1444498560}]}";
-  Serial.println("Isaac....." + jsonRequest);
+  jsonRequest = jsonRequest + "\"ground_humidity\": [{ \"value\": " + sensorInt + ", \"timestamp\": 1446723485}]}";
   Serial.print(sensorInt);
   Serial.print("%");
   Serial.println();
@@ -125,7 +115,12 @@ void luchtVochtTemp() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
   
-  jsonRequest = jsonRequest + "\"temperature\": [{ \"value\": 6,\"unit\": \"C\", \"timestamp\": 1444498560}], \"air_humidity\": [{ \"value\": 70, \"timestamp\": 1444498560}],";
+  char charVal[10];
+  String airTemperature = dtostrf(t, 1, 1, charVal);
+  String airHumidity = dtostrf(h, 1, 1, charVal);
+  time_t now = now();
+  
+  jsonRequest = jsonRequest + "\"temperature\": [{ \"value\": 30,\"unit\": \"C\", \"timestamp\": 1446723485}], \"air_humidity\": [{ \"value\": 50, \"timestamp\": 1446723485}],";
 
   Serial.print("Air Humidity: ");
   Serial.print(h);
@@ -142,24 +137,25 @@ void luchtVochtTemp() {
   Serial.println("F");
 }
 
+/**
+  Method to postData to Webservice
+*/
 void postData() {
-  char = "\"temperature\": [{ \"value\": 6,\"unit\": \"C\", \"timestamp\": 1444498560}], \"air_humidity\": [{ \"value\": 70, \"timestamp\": 1444498560}], \"ground_humidity\": [{ \"value\": 80, \"timestamp\": 1444498560}]}";
+  //String jsonRequest = "{\"temperature\": [{ \"value\": 6,\"unit\": \"C\", \"timestamp\": 1444498560}], \"air_humidity\": [{ \"value\": 70, \"timestamp\": 1444498560}], \"ground_humidity\": [{ \"value\": 80, \"timestamp\": 1444498560}]}";
     
-  if (client.connect("45.55.40.47/planten/create",80)) { // REPLACE WITH YOUR SERVER ADDRESS
+  if (client.connect(server, serverPort)) { // REPLACE WITH YOUR SERVER ADDRESS
     Serial.println(F("Making HTTP request..."));
-    
-    client.println("POST /create HTTP/1.1");
-    client.println("Host: 45.55.40.47/planten/create"); // SERVER ADDRESS HERE TOO
+    client.println("POST /planten/create HTTP/1.1");
+    client.println("Host: funda.ga"); // SERVER ADDRESS HERE TOO
     client.println("User-Agent: Arduino/1.0");
     client.println("Connection: close");
     client.println("Content-Type: application/json charset=UTF-8"); 
-    client.print("Content-Length: ");    
+    client.print("Content-Length: ");
     client.println(jsonRequest.length()); 
-    client.println(); 
-    client.print(URLEncode(jsonRequest)); 
+    client.println();
+    client.print(jsonRequest); 
     Serial.println("posted...");
   } else {
-    // If you couldn't make a connection:
     Serial.println("Connection failed to post");
     Serial.println("Disconnecting.");
     client.stop();
@@ -170,22 +166,3 @@ void postData() {
   }
 }
 
-String URLEncode(const char* msg)
-{
-    const char *hex = "0123456789abcdef";
-    String encodedMsg = "";
-
-    while (*msg!='\0'){
-        if( ('a' <= *msg && *msg <= 'z')
-                || ('A' <= *msg && *msg <= 'Z')
-                || ('0' <= *msg && *msg <= '9') ) {
-            encodedMsg += *msg;
-        } else {
-            encodedMsg += '%';
-            encodedMsg += hex[*msg >> 4];
-            encodedMsg += hex[*msg & 15];
-        }
-        msg++;
-    }
-    return encodedMsg;
-}
